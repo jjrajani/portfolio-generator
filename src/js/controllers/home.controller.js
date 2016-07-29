8815241c3ac9a393e63acd69b56c560f25e39a92
humdrum <jjrajani@gmail.com>
2016-07-29 15:31:36 -0400

    added jumbotron carousel

--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -1,7 +1,22 @@
 function HomeController() {
 
   let vm = this;
+  vm.jumbotron = jumbotron;
+  vm.images = ['http://i.imgur.com/jB68hH5.png', 'https://www.placecage.com/200/200']
+  vm.jumbotron = jumbotron;
   
+  function jumbotron() {
+    let img = document.querySelector('.jumbotron');
+    let i = 0;
+    setInterval(function () {
+      img.src = vm.images[i];
+      i++
+      if (i >= vm.images.length) {
+        i = 0;
+      }
+    }, 2000)
+  }
+
 }
 
 HomeController.$inject = [];
